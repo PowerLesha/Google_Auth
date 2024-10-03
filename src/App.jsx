6ddc3347589558acde3f8db5e514cd46da40a581
Lesha <leshapowerlifter@gmail.com>
2024-10-03 14:41:07 +0300

    added basic login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
   return (
     <>
-      <h1>Google Auth</h1>
-      <div className="card"></div>
+      <Login />
     </>
   );
 }
